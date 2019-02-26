4b05cdf249cf221096d533ef59cc8d17664f99e2
Rangel Netto <j.rangel.flash@gmail.com>
2019-02-26 01:33:12 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = 'mongodb://rangel:aa15442663@ds129625.mlab.com:29625/auth';
+const MONGO_URI = '';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
